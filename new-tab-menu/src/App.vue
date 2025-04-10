834d5c4bc6e14b1c76476b396eb31e5b91cc04b0
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-04-10 09:39:16 +0200

    first commit

---
+++ new-tab-menu/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+import './assets/styles/styles.css';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ new-tab-menu/src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Marcadores</h1>
+    <p v-for="n in 10" :key="n">Marcador número {{ n }}</p>
+  </div>
+</template>
